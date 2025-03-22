e29ddec1c843d3f8782c8be26c38d315b3cc35c9
dogevevo <rstevenpr@gmail.com>
2025-03-21 23:15:38 -0600

    second commit

---
+++ src/components /AnimatedTitle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AnimatedTitle = () => {
+  return (
+        <div className="about-subtext">
+            <p>The Game of Games begins-your life, now an epic MMORPG</p>
+            <p> Zentry unites every player from countless games and platforms </p>
+        </div>
+    )
+}
+
+export default AnimatedTitle
--- src/components /Hero.jsx
+++ src/components /Hero.jsx
@@ -1,5 +1,11 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button'
+import { TiLocation, TiLocationArrow } from 'react-icons/ti'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
 
@@ -23,10 +29,70 @@ const Hero = () => {
     setCurrentIndex( upcomingVideoIndex )
   }
 
+  useEffect(() => { 
+    if (loadedVideos == totalVideos - 1 ) {
+      setIsLoanding(false)
+    }
+  }, [loadedVideos])
+
+  useGSAP(  () => {
+    if (hasClicked) {
+      gsap.set('#next-video', { visibility: 'visible'})
+
+      gsap.to('#next-video', {
+        transformOrigin: 'center center', 
+        scale: 1, 
+        width: '100%', 
+        height: '100%', 
+        duration: 1, 
+        ease: 'power1.inOut',
+        onStart : () => nextVideoRef.current.play()
+      })
+
+      gsap.from('#current-video', {
+        transformOrigin: 'center center', 
+        scale: 0, 
+        duration:1.5,
+        ease: 'power1.inOut'
+      })
+    }
+  }, {dependencies: [currentIndex], revertOnUpdate: true })
+
+  useGSAP( () => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+      borderRadius: "0% 0% 40% 10%",
+    });
+
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    })
+  })
+
   const getVideoSrc = (index) => `public/videos/hero-${index}.mp4`
 
   return (
-    <div className='relative h-dvh w-screen overflow-x-hidden'> 
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+
+      {isLoanding && (
+        <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50' > 
+          <div className="three-body">
+            <div className='three-body__dot' />
+            <div className='three-body__dot' />
+            <div className='three-body__dot' />
+            <div/>
+          </div>
+        </div>
+      )}
+ 
         <div id='video-frame' className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
             <div>
                 <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-xl">
@@ -44,11 +110,12 @@ const Hero = () => {
                   <div className="mt-24 px-5 sm:px-10">
                     <h1 className='special-font hero-heading text-blue-100' >redefi<b>n</b>e</h1>
                     <p className='mb-5 max-w-64 font-robert-regular text-blue-100' > Enter The Metagame Layer <br /> Unleash The Play Economy </p>
-                    <Button/>
+                    <Button id="watch-trailer" title="Watch Trailer" leftIcon={ < TiLocationArrow /> } containerClass="!bg-yellow-300 flex-center gap-1"  />
                   </div>
                 </div>
             </div>
         </div>
+      <h1 className='special-font hero-heading absolute bottom-5 right-5 text-black'>G<b>a</b>ming</h1>
     </div>
   )
 }
